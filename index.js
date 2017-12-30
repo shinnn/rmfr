95e4971de7c94638e3dad765f08db03aa4b2c904
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2017-12-30 19:42:57 +0900

    update dependencies and devDependencies
    Highlights:
    
    * update ESLint from v3 to v4
    * update @shinnn/eslint-config and follow its coding rules, for exmaple tab indent
    * switch from istanbul to nyc

--- index.js
+++ index.js
@@ -6,85 +6,83 @@ const fs = require('graceful-fs');
 const rimraf = require('rimraf');
 
 const fsMethods = [
-  'unlink',
-  'chmod',
-  'stat',
-  'lstat',
-  'rmdir',
-  'readdir'
+	'unlink',
+	'chmod',
+	'stat',
+	'lstat',
+	'rmdir',
+	'readdir'
 ];
 const defaultGlobOptions = {
-  nosort: true,
-  silent: true,
-  // Remove this line when isaacs/rimraf#133 is merged
-  absolute: true
+	nosort: true,
+	silent: true,
+	// Remove this line when isaacs/rimraf#133 is merged
+	absolute: true
 };
 const RIMRAF_DOC_URL = 'https://github.com/isaacs/rimraf#options';
 
 module.exports = function rmfr(filePath, options) {
-  if (options) {
-    if (typeof options !== 'object') {
-      return Promise.reject(new TypeError(
-        inspect(options) +
-        ` is not an object. Expected an option object passed to rimraf ${RIMRAF_DOC_URL}.`
-      ));
-    }
-  } else {
-    options = {};
-  }
+	if (options) {
+		if (typeof options !== 'object') {
+			return Promise.reject(new TypeError(`${inspect(options)
+			} is not an object. Expected an option object passed to rimraf ${RIMRAF_DOC_URL}.`));
+		}
+	} else {
+		options = {};
+	}
 
-  const errors = [];
+	const errors = [];
 
-  options = Object.assign({
-    glob: false,
-    unlink: fs.unlink,
-    chmod: fs.chmod,
-    rmdir: fs.rmdir,
-    readdir: fs.readdir
-  }, options);
+	options = Object.assign({
+		glob: false,
+		unlink: fs.unlink,
+		chmod: fs.chmod,
+		rmdir: fs.rmdir,
+		readdir: fs.readdir
+	}, options);
 
-  for (const method of fsMethods) {
-    if (options[method] !== undefined && typeof options[method] !== 'function') {
-      errors.push(`\`${method}\` option must be a function, but got ${inspect(options[method])} (${typeof options[method]}).`);
-    }
-  }
+	for (const method of fsMethods) {
+		if (options[method] !== undefined && typeof options[method] !== 'function') {
+			errors.push(`\`${method}\` option must be a function, but got ${inspect(options[method])} (${typeof options[method]}).`);
+		}
+	}
 
-  if (options.maxBusyTries !== undefined && typeof options.maxBusyTries !== 'number') {
-    errors.push(`\`maxBusyTries\` option must be a number, but got ${inspect(options.maxBusyTries)} (${typeof options.maxBusyTries}).`);
-  }
+	if (options.maxBusyTries !== undefined && typeof options.maxBusyTries !== 'number') {
+		errors.push(`\`maxBusyTries\` option must be a number, but got ${inspect(options.maxBusyTries)} (${typeof options.maxBusyTries}).`);
+	}
 
-  if (options.emfileWait !== undefined && typeof options.emfileWait !== 'number') {
-    errors.push(`\`emfileWait\` option must be a number, but got ${inspect(options.emfileWait)} (${typeof options.emfileWait}).`);
-  }
+	if (options.emfileWait !== undefined && typeof options.emfileWait !== 'number') {
+		errors.push(`\`emfileWait\` option must be a number, but got ${inspect(options.emfileWait)} (${typeof options.emfileWait}).`);
+	}
 
-  if (options.glob === true) {
-    options.glob = defaultGlobOptions;
-  } else if (options.glob !== false && typeof options.glob !== 'object') {
-    errors.push(`\`glob\` option must be an object passed to \`glob\` or a Boolean value, but got ${inspect(options.glob)} (${typeof options.glob}).`);
-  }
+	if (options.glob === true) {
+		options.glob = defaultGlobOptions;
+	} else if (options.glob !== false && typeof options.glob !== 'object') {
+		errors.push(`\`glob\` option must be an object passed to \`glob\` or a Boolean value, but got ${inspect(options.glob)} (${typeof options.glob}).`);
+	}
 
-  if (options.disableGlob !== undefined && typeof options.disableGlob !== 'boolean') {
-    errors.push(`\`disableGlob\` option must be a boolean, but got ${inspect(options.disableGlob)} (${typeof options.disableGlob}).`);
-  }
+	if (options.disableGlob !== undefined && typeof options.disableGlob !== 'boolean') {
+		errors.push(`\`disableGlob\` option must be a boolean, but got ${inspect(options.disableGlob)} (${typeof options.disableGlob}).`);
+	}
 
-  if (errors.length === 1) {
-    return Promise.reject(new TypeError(`${errors[0]} ${RIMRAF_DOC_URL}`));
-  }
+	if (errors.length === 1) {
+		return Promise.reject(new TypeError(`${errors[0]} ${RIMRAF_DOC_URL}`));
+	}
 
-  if (errors.length !== 0) {
-    return Promise.reject(new TypeError(`There was ${errors.length} errors in rimraf options you provided:
-${errors.map(error => '  * ' + error).join('\n')}
+	if (errors.length !== 0) {
+		return Promise.reject(new TypeError(`There was ${errors.length} errors in rimraf options you provided:
+${errors.map(error => `  * ${error}`).join('\n')}
 Read ${RIMRAF_DOC_URL} for the details.`));
-  }
+	}
 
-  return new Promise((resolve, reject) => {
-    rimraf(filePath, options, err => {
-      if (err) {
-        reject(err);
-        return;
-      }
+	return new Promise((resolve, reject) => {
+		rimraf(filePath, options, err => {
+			if (err) {
+				reject(err);
+				return;
+			}
 
-      resolve();
-    });
-  });
+			resolve();
+		});
+	});
 };
--- test.js
+++ test.js
@@ -1,73 +1,107 @@
 'use strict';
 
-const pathExists = require('path-exists');
+const {promisify} = require('util');
+
+const {lstat, writeFile} = require('graceful-fs');
 const rmfr = require('.');
 const test = require('tape');
-const writeFileAtomically = require('write-file-atomically');
 
-test('rmfr()', t => {
-  t.plan(9);
+const promisifiedLstat = promisify(lstat);
+const promisifiedWriteFile = promisify(writeFile);
 
-  writeFileAtomically('tmp_file_for_test', '', () => {
-    rmfr('tmp_file_for_test').then(res => {
-      pathExists('tmp_file_for_test').then(exists => t.notOk(exists, 'should remove a file.'));
-      t.strictEqual(res, undefined, 'should pass no value to the onFulfilled function.');
-    });
-  }).catch(t.fail);
+test('rmfr()', async t => {
+	await promisifiedWriteFile('tmp_file_for_test', '');
+	await rmfr('tmp_file_for_test');
 
-  rmfr('inde*.js', {}).then(() => {
-    pathExists('index.js').then(exists => t.ok(exists, 'should disable `glob` option by default.'));
-  }).catch(t.fail);
+	try {
+		await promisifiedLstat('tmp_file_for_test');
+		t.fail('File not removed.');
+	} catch ({code}) {
+		t.equal(
+			code,
+			'ENOENT',
+			'should remove a file.'
+		);
+	}
 
-  rmfr('.gitignore', {unlink: (path, cb) => cb(new Error('Ouch'))}).then(t.fail, err => {
-    t.strictEqual(
-      err.message,
-      'Ouch',
-      'should fail when an error occurs while calling rimraf.'
-    );
-  }).catch(t.fail);
+	await rmfr('inde*.js', {});
 
-  rmfr().then(t.fail, err => {
-    t.strictEqual(
-      err.message,
-      'rimraf: missing path',
-      'should fail when it takes no arguments.'
-    );
-  }).catch(t.fail);
+	t.ok(
+		await promisifiedLstat('index.js'),
+		'should disable `glob` option by default.'
+	);
 
-  rmfr(['1'], {glob: true}).then(t.fail, err => {
-    t.strictEqual(
-      err.message,
-      'rimraf: path should be a string',
-      'should fail when the first argument is not a string.'
-    );
-  }).catch(t.fail);
+	const error = new Error('_');
 
-  rmfr('foo', 1).then(t.fail, err => {
-    t.ok(
-      err.message.includes('1 is not an object'),
-      'should fail when the second argument is not an object.'
-    );
-  }).catch(t.fail);
+	try {
+		await rmfr('.gitignore', {unlink: (path, cb) => cb(error)});
+		t.fail('Unexpectedly succeeded.');
+	} catch (err) {
+		t.equal(
+			err,
+			error,
+			'should fail when an error occurs while calling rimraf.'
+		);
+	}
 
-  rmfr('foo', {chmod: new Set(['a'])}).then(t.fail, err => {
-    t.strictEqual(
-      err.name,
-      'TypeError',
-      'should fail when it takes an invalid option.'
-    );
-  }).catch(t.fail);
+	try {
+		await rmfr();
+		t.fail('Unexpectedly succeeded.');
+	} catch ({message}) {
+		t.equal(
+			message,
+			'rimraf: missing path',
+			'should fail when it takes no arguments.'
+		);
+	}
 
-  rmfr('foo', {
-    maxBusyTries: 'foo',
-    emfileWait: 'bar',
-    glob: 'baz',
-    disableGlob: 'qux'
-  }).then(t.fail, err => {
-    t.strictEqual(
-      err.name,
-      'TypeError',
-      'should fail when it takes invalid options.'
-    );
-  }).catch(t.fail);
+	try {
+		await rmfr(['1'], {glob: true});
+		t.fail('Unexpectedly succeeded.');
+	} catch ({message}) {
+		t.equal(
+			message,
+			'rimraf: path should be a string',
+			'should fail when the first argument is not a string.'
+		);
+	}
+
+	try {
+		await rmfr('foo', 1);
+		t.fail('Unexpectedly succeeded.');
+	} catch ({message}) {
+		t.ok(
+			message.includes('1 is not an object'),
+			'should fail when the second argument is not an object.'
+		);
+	}
+
+	try {
+		await rmfr('foo', {chmod: new Set(['a'])});
+		t.fail('Unexpectedly succeeded.');
+	} catch ({name}) {
+		t.equal(
+			name,
+			'TypeError',
+			'should fail when it takes an invalid option.'
+		);
+	}
+
+	try {
+		await rmfr('foo', {
+			maxBusyTries: 'foo',
+			emfileWait: 'bar',
+			glob: 'baz',
+			disableGlob: 'qux'
+		});
+		t.fail('Unexpectedly succeeded.');
+	} catch ({name}) {
+		t.equal(
+			name,
+			'TypeError',
+			'should fail when it takes invalid options.'
+		);
+	}
+
+	t.end();
 });
